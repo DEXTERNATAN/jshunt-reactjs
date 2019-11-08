48d2cdffbbcad47f84a6a45f42032594a168d95e
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2019-11-08 16:29:15 -0300

    Adiciona tela de detalhe do produto

---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+// A maneira como este componente foi escrito é um stateless componente
+const Header = () => <header id="main-header">JSHunt</header>;
+
+export default Header;
